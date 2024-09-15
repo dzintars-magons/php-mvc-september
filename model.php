af2a861e54b9ddebf048d58012f831ae2dfbda14
Dzintars <dzintars.magons@gmail.com>
2024-09-15 16:02:59 +0300

    mvc pattern - basic application

---
+++ model.php
@@ -0,0 +1,17 @@
+<?php
+
+class Model
+{
+    public function getData(): array
+    {
+        $dsn = "mysql:host=localhost;dbname=product_db_september;charset=utf8;port=3306";
+
+        $pdo = new PDO($dsn, "product_db_september", "secret", [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+
+        $stmt = $pdo->query("SELECT * FROM product");
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
